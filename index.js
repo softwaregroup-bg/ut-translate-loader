42a22e71031b42c8d587499dd59c479959e6375d
Georgi Alexandrov <Georgi.alexandrov@Softwaregroup-bg.com>
2016-05-30 16:43:27 +0300

    feat: refactor to work with new database schema

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 module.exports = function(source) {
+    var translateCfg = JSON.parse(source);
     var callback = this.async();
     this.options.closures.translate && this.options.closures.translate({
-        module: source
+        language: translateCfg.devLanguage
     }).then(function(response) {
+        if (typeof response === 'string') {
+            var PO = require('pofile');
+            PO.load(response, function(err, po) {
+                if (err) throw err;
+                var translations = po.items.reduce((languages, msg) => {
+                    languages[msg.msgid] = msg.msgstr[0] || msg.msgid;
+                    return languages;
+                }, {});
+                callback(null, 'module.exports = ' + JSON.stringify(translations, undefined, '\t') + ';');
+            });
+        }
         var translations = response[0].reduce((languages, msg) => {
-            if (!languages[msg.language]) languages[msg.language] = {};
-            languages[msg.language][msg.msgid] = msg.msgstr;
+            languages[msg.dictionaryKey] = msg.translatedValue;
             return languages;
         }, {});
         callback(null, 'module.exports = ' + JSON.stringify(translations, undefined, '\t') + ';');
